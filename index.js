5ee0f4549977740834ed3efd669e2b31826b6007
Sopagnarith Seam <45729914+pagnarithseam18@users.noreply.github.com>
2021-10-25 14:43:33 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const pingpong = (schedules) => {
-    var arr = schedules.sort((a, b) => {
+  if (Array.isArray(schedules))
+  {
+    schedules = schedules.sort((a, b) => {
         if (a[0] == b[0])
             return a[1] - b[1]
         return a[0] - b[0]
@@ -10,25 +12,30 @@ const pingpong = (schedules) => {
 
     for (let time = 9; time < 18; time++)
     {
-        filler1 = arr
-        filler2 = table
-        filler2.forEach((element) => {
-            if (element[1] <= time)
-                table = table.filter((e) => e != element)
-        })
-        filler1.forEach((element) => {
-            if(element[0] > time)
-                return
-            table.push(element)
-            arr = arr.slice(1,)
-        })
-        
-        if (table.length >= 2 && table.length < 4)
-            sessions[0] += 1
-        else if (table.length >=4)
-            sessions[1] += 1
+      filler1 = schedules
+      filler2 = table
+      filler2.forEach((element) => {
+          if (element[1] <= time)
+              table = table.filter((e) => e != element)
+      })
+      filler1.forEach((element) => {
+          if(element[0] > time)
+              return
+          table.push(element)
+          schedules = schedules.slice(1,)
+      })
+      
+      if (table.length >= 2 && table.length < 4)
+          sessions[0] += 1
+      else if (table.length >=4)
+          sessions[1] += 1
     }
     console.log(sessions)
+  }
+}
+
+const checkValidity = (schedules) => {
+  
 }
 
 pingpong([[9,10], [9,12], [13,18], [16,18], [14,18], [10,13], [15,18]])
